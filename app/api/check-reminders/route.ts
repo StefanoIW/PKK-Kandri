6888a05199de7d133cceb93a925f7bd45c19d11d
stefano indra wibowo <105740282+StefanoIW@users.noreply.github.com>
2025-06-21 13:19:36 +0700

    Update route.ts

--- app/api/check-reminders/route.ts
+++ app/api/check-reminders/route.ts
@@ -79,7 +79,7 @@ Terima kasih. 🙏`
       const response = await fetch("https://api.fonnte.com/send", {
         method: "POST",
         headers: {
-          Authorization: "pUHPiTDPi4aeGQo9Q4PW",
+          Authorization: "KChLR9EBA65W2eaqkwBV",
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
